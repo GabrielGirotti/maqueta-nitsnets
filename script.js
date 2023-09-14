915579fccdaa412ae577cec13abff769a7066ca5
GabrielGirotti <girottigabriel@gmail.com>
2023-09-14 17:17:01 -0300

    Mejorando funcionalidad carruseles

--- script.js
+++ script.js
@@ -1,18 +1,50 @@
-const productContainers = [
-  ...document.querySelectorAll(".carrusel__container"),
-];
-const nxtBtn = [...document.querySelectorAll(".carrusel__next")];
-const preBtn = [...document.querySelectorAll(".carrusel__prev")];
+/* --------- FUNCION CARRUSEL 1 --------- */
 
-productContainers.forEach((item, i) => {
-  let containerDimensions = item.getBoundingClientRect();
-  let containerWidth = containerDimensions.width;
+const carrusel = document.querySelector(".carrusel__container");
+const nxtBtn = document.querySelector(".carrusel__next");
+const preBtn = document.querySelector(".carrusel__prev");
 
-  nxtBtn[i].addEventListener("click", () => {
-    item.scrollLeft += containerWidth;
-  });
+nxtBtn.addEventListener("click", () => {
+  carrusel.scrollLeft += carrusel.offsetWidth;
+});
 
-  preBtn[i].addEventListener("click", () => {
-    item.scrollLeft -= containerWidth;
-  });
+preBtn.addEventListener("click", () => {
+  carrusel.scrollLeft -= carrusel.offsetWidth;
+});
+
+/* --------- FUNCION CARRUSEL 2 --------- */
+
+const carruselS = document.querySelector(".carrusel__container-small");
+const nxtBtnS = document.querySelector(".carrusel__next-small");
+
+nxtBtnS.addEventListener("click", () => {
+  carruselS.scrollLeft += carruselS.offsetWidth;
+});
+
+/* --------- FUNCION CARRUSEL 3 --------- */
+
+const carruselTres = document.getElementById("carrusel-3");
+const nxtBtnTres = document.getElementById("nxt3");
+const preBtnTres = document.getElementById("prev3");
+
+nxtBtnTres.addEventListener("click", () => {
+  carruselTres.scrollLeft += carruselTres.offsetWidth;
+});
+
+preBtnTres.addEventListener("click", () => {
+  carruselTres.scrollLeft -= carruselTres.offsetWidth;
+});
+
+/* --------- FUNCION El deporte es para todos --------- */
+
+const carruselL = document.querySelector(".carrusel__container-gap-16");
+const nxtBtnL = document.getElementById("nxtL");
+const preBtnL = document.getElementById("prevL");
+
+nxtBtnL.addEventListener("click", () => {
+  carruselL.scrollLeft += carruselL.offsetWidth;
+});
+
+preBtnL.addEventListener("click", () => {
+  carruselL.scrollLeft -= carruselL.offsetWidth;
 });
